12ea97a7cb8395070284d051c29975acf8dd0a69
RomanAkberov <r.k.akberov@gmail.com>
2020-11-07 20:11:13 +0100

    Adjective declension

--- docs/index.js
+++ docs/index.js
@@ -493,7 +493,7 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper868 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper870 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 251, __wbg_adapter_16);
         return addHeapObject(ret);
     };
--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn build_string_from_parts(parts: &[Option<&str>]) -> String {
     let mut string = String::new();
     for part in parts {
         if let Some(part) = part {
-            if !part.starts_with(",") {
+            if string.len() > 0 && !part.starts_with(",") {
                 string.push(' ');
             }
             string.push_str(part);
@@ -97,11 +97,159 @@ const WEIRD: &[&str] = &[
     "любопытный",
 ];
 
+pub enum Number {
+    Singular,
+    Plural,
+}
+
+pub enum HardAdjDeclension {
+    Y,
+    O,
+}
+
+pub enum MixedAdjDeclension {
+    I,
+    O,
+}
+
+pub enum AdjDeclension {
+    Hard(HardAdjDeclension),
+    Mixed(MixedAdjDeclension),
+    Soft,
+}
+
+fn adjective_declension(adjective: &str) -> AdjDeclension {
+    const MIXED_STEM_ENDS: &[char] = &['г', 'к', 'х', 'ш', 'ч', 'щ', 'ц'];
+
+    let mut chars = adjective.chars().rev();
+    let ending_start = chars.nth(1).unwrap();
+    if ending_start == 'ы' {
+        return AdjDeclension::Hard(HardAdjDeclension::Y);
+    }
+    let is_mixed = MIXED_STEM_ENDS.contains(&chars.next().unwrap());
+    if ending_start == 'о' {
+        if is_mixed { 
+            AdjDeclension::Mixed(MixedAdjDeclension::O)
+        } else { 
+            AdjDeclension::Hard(HardAdjDeclension::O)
+        }
+    } else if is_mixed {
+        AdjDeclension::Mixed(MixedAdjDeclension::I)
+    } else {
+        AdjDeclension::Soft
+    }
+}
+
+fn adjective_ending(declension: AdjDeclension, gender: Gender, animacy: Animacy, case: Case, number: Number) -> &'static str {
+    use AdjDeclension::*;
+    use Animacy::*;
+    use Case::*;
+    use Gender::*;
+
+    match number {
+        Number::Plural => match declension {
+            Hard(_) => match case {
+                Nominative => "ые",
+                Genitive => "ых",
+                Dative => "ым",
+                Accusative => match animacy {
+                    Animate => "ых",
+                    Inanimate => "ые",
+                }
+                Instrumental => "ыми",
+                Locative => "ых",
+            } 
+            Mixed(_) | Soft => match case {
+                Nominative => "ие",
+                Genitive => "их",
+                Dative => "им",
+                Accusative => match animacy {
+                    Animate => "их",
+                    Inanimate => "ие",
+                }
+                Instrumental => "ими",
+                Locative => "их",
+            }
+        }
+        Number::Singular => {
+            let (nominative, instrumental) = match declension {
+                Hard(HardAdjDeclension::Y) => ("ый", "ым"),
+                Hard(HardAdjDeclension::O) => ("ой", "ым"),
+                Mixed(MixedAdjDeclension::O) => ("ой", "им"),
+                Mixed(MixedAdjDeclension::I) | Soft => ("ий", "им"),
+            };
+            match gender {
+                Masculine => match declension {
+                    Hard(_) | Mixed(_) => match case {
+                        Nominative => nominative,
+                        Genitive => "ого",
+                        Dative => "ому",
+                        Accusative => match animacy {
+                            Animate => "ого",
+                            Inanimate => nominative,
+                        }
+                        Instrumental => instrumental,
+                        Locative => "ом",
+                    }
+                    Soft => match case {
+                        Nominative => nominative,
+                        Genitive => "его",
+                        Dative => "ему",
+                        Accusative => match animacy {
+                            Animate => "его",
+                            Inanimate => nominative,
+                        }
+                        Instrumental => instrumental,
+                        Locative => "им",
+                    }
+                }
+                Feminine => match declension {
+                    Hard(_) | Mixed(_) => match case {
+                        Nominative => "ая",
+                        Accusative => "ую",
+                        _ => "ой",
+                    }
+                    Soft => match case {
+                        Nominative => "яя",
+                        Accusative => "юю",
+                        _ => "ей",
+                    }
+                }
+                Neuter => match declension {
+                    Hard(_) | Mixed(_) => match case {
+                        Nominative => "ое",
+                        Genitive => "ого",
+                        Dative => "ому",
+                        Accusative => "ое",
+                        Instrumental => instrumental,
+                        Locative => "ом",
+                    }
+                    Soft => match case {
+                        Nominative => "ее",
+                        Genitive => "его",
+                        Dative => "ему",
+                        Accusative => "ее",
+                        Instrumental => instrumental,
+                        Locative => "ем",
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn decline_adjective(adjective: &str, gender: Gender, animacy: Animacy, case: Case, number: Number) -> String {
+    let len = adjective.chars().count();
+    let mut string: String = adjective.chars().take(len - 2).collect();
+    string.push_str(adjective_ending(adjective_declension(adjective), gender, animacy, case, number));
+    string
+}
+
 const OBJECTS: &[Noun] = &[
     Noun::new("птица", Gender::Feminine, Animacy::Animate),
     Noun::new("лягушка", Gender::Feminine, Animacy::Animate),
     Noun::new("осьминог", Gender::Masculine, Animacy::Inanimate),
-    Noun::new("череп", Gender::Feminine, Animacy::Inanimate),
+    Noun::new("череп", Gender::Masculine, Animacy::Inanimate),
     Noun::new("рог", Gender::Masculine, Animacy::Inanimate),
     Noun::new("игрушка", Gender::Feminine, Animacy::Inanimate),
     Noun::new("орнитоптер", Gender::Masculine, Animacy::Inanimate),
@@ -115,6 +263,7 @@ const OBJECTS: &[Noun] = &[
 
 pub struct Context {
     pub gender: Gender,
+    pub animacy: Animacy,
     pub rng: ThreadRng,
 }
 
@@ -127,7 +276,8 @@ fn interpret(call: &str, context: &mut Context) -> Result<String, InterpreterErr
     let func_and_args: Vec<_> = call.split(":").collect();
     match func_and_args[0] {
         "adj" => {
-            Ok(WEIRD.choose(&mut context.rng).unwrap().to_string())
+            let adjective = WEIRD.choose(&mut context.rng).unwrap(); 
+            Ok(decline_adjective(adjective, context.gender, context.animacy, Case::Nominative, Number::Singular))
         }
         _ => Err(InterpreterError::UnknownFunction)
     }
@@ -136,6 +286,16 @@ fn interpret(call: &str, context: &mut Context) -> Result<String, InterpreterErr
 fn run(text: &str, context: &mut Context) -> String {
     let regex = regex::Regex::new("\\[([^\\]]+)\\]").unwrap();
     regex.replace_all(text, |captures: &regex::Captures| interpret(captures.get(1).unwrap().as_str(), context).unwrap()).into_owned()
+}
+
+fn capitalize(s: &str) -> String {
+    let mut chars = s.chars();
+    match chars.next() {
+        None => String::new(),
+        Some(ch) => {
+            ch.to_uppercase().chain(chars).collect()
+        }
+    }
 }
 
 pub enum Msg {
@@ -162,9 +322,12 @@ impl Component for Model {
                 let mut oddity = Oddity::default();
                 let object = OBJECTS.choose(&mut rng).unwrap();
                 let gender = object.gender;
+                let animacy = object.animacy;
                 oddity.object = String::from(object.text);
                 oddity.weird = Some(String::from("[adj]"));
-                self.value = run(&build_oddity_string(&oddity), &mut Context { gender, rng });
+                self.value = run(&build_oddity_string(&oddity), &mut Context { gender, animacy, rng });
+                self.value = capitalize(&self.value);
+                self.value.push('.');
                 true
             }
         }
